cbbef92f7d2b10546c28983c944a7b29bba89d57
Abhishek Nair <38916532+mrabhi05@users.noreply.github.com>
2021-01-26 00:24:48 +0530

    Assignment 4 Q1 Done

---
+++ Day 5/day5.js
@@ -0,0 +1,12 @@
+
+
+
+ 
+
+
+function add(firstnum, secondnum){
+        var firstnum = document.getElementById("first").value;
+        var secondnum = document.getElementById("second").value;  
+        
+        console.log(firstnum + secondnum)
+}
