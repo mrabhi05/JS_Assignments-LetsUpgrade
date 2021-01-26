197dc7a509121e7d5985fc74e1bde64e95133932
Abhishek Nair <38916532+mrabhi05@users.noreply.github.com>
2021-01-26 12:45:01 +0530

    Day 5 - Assignment 4 Completed

--- Day 5/day5.js
+++ Day 5/day5.js
@@ -5,8 +5,11 @@
 
 
 function add(firstnum, secondnum){
-        var firstnum = document.getElementById("first").value;
-        var secondnum = document.getElementById("second").value;  
+        var firstnum = document.getElementById('first').value;
+        var secondnum = document.getElementById('second').value;  
+
+        firstnum = Number(firstnum)
+        secondnum = Number(secondnum)
         
         console.log(firstnum + secondnum)
 }
